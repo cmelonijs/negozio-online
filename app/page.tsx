e4e1bb9898d0a67c348e257d5d87966e9cb67077
cmelonijs <carlomeloni63@gmail.com>
2025-03-19 12:37:42 +0100

    feat: first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+  return (
+    <>
+      <h1>Negozio Online</h1>
+      <Button>Push the button</Button>
+    </>
+  );
+};
+
+export default Homepage;
