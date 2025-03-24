1b2389333a50842d39ec669bffe13ff7ea30736d
Alba Castilla <castillamackin@gmail.com>
2025-03-24 16:47:14 +0100

    feat: logo possitioned

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+
 const Header = () => {
-  return <>Header</>;
+    return(
+        <div className="flex items-center ">
+            <div className="logoTotal pt-6 gap-3 pl-6 pb-6 flex items-center">
+                <Image
+                  src="/logo.png"
+                  width={100}
+                  height={20}
+                  className="md:block"
+                  alt="Logo version 1"
+                />
+
+<h1 className="text-xl font-bold font-rubik-rounded">Empresa | E-commerce</h1>
+
+                {/* <Button>Push the button</Button> */}
+
+                </div>
+              </div>
+    );
 };
 
 export default Header;
