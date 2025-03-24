8f0fbe11999614e6429da05c4092a6451d8de4bc
AlbaCastilla <castillamacki@gmail.com>
2025-03-24 10:27:13 +0100

    Revert "Open Sans font"
    This reverts commit 849d4bf9865d5aff14bba6d04bb980cdc1d6bc86.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import{Open_Sans} from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const open_sans = Open_Sans ({subsets: ["latin"], weight: ['500']  });
 
 export const metadata: Metadata = {
   title: "Store",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${open_sans.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
