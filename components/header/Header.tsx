e4febf039956d6da6119d0db9b60ba8a9dd0aaa2
Alba Castilla <castillamackin@gmail.com>
2025-03-24 14:23:18 +0100

    feat: logo centered better

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,17 +2,20 @@ import Image from "next/image";
 
 const Header = () => {
     return(
-        <div className="flex ">
+        <div className="flex items-center  bg-blue-500">
+            <div className="logoTotal pt-6 pl-6 pb-6 flex items-center">
                 <Image
                   src="/logo.png"
                   width={100}
                   height={20}
-                  className="hidden md:block"
+                  className="md:block"
                   alt="Logo version 1"
                 />
         
                 <h1>Empresa | Store</h1>
                 {/* <Button>Push the button</Button> */}
+              
+                </div>
               </div>
     );
 };
