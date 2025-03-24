1fb308f6ec7b14b58ac63a81d482ec987bfa6037
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-24 15:12:24 +0100

    added footer

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+
+    return (
+        <footer className="border-t">
+            <div className="p-5 flex-center">
+                <h2>
+                    2025
+                </h2>
+                <h2>
+                    Companny name
+                </h2>
+                <h2>
+                . All right are reserved.
+                </h2>
+            </div>
+        </footer>
+    );
+
+};
+export default Footer;
