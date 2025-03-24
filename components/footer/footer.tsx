f817ea6d45ca0f09e89446273d85984fa2a7ad76
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-24 16:28:40 +0100

    footer updated

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <>Footer</>;
-};
 
+  return (
+      <footer className="border-t">
+          <div className="p-5 flex-center gap-2">
+              <h2>
+                  2025
+              </h2>
+              <h2>
+                  Companny name
+              </h2>
+              <h2>
+              . All right are reserved.
+              </h2>
+          </div>
+      </footer>
+  );
+
+};
 export default Footer;
