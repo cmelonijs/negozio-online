affe62e0c46256447479552e54424ed9563f7636
Alba Castilla <castillamackin@gmail.com>
2025-03-25 17:33:10 +0100

    fix: minor architecture fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,14 @@
 import sampleData from "@/db/sample-data";
-import ProductCard from "@/components/shared/product/product-card";
+import ProductList from "@/components/shared/product/product-list";
 
 const RootPage = () => {
   return (
     <>
       <div className="product-cards-bigdiv">
-        <div className="cards grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-          {sampleData.products.slice(0, 4).map((product) => (
-            <ProductCard key={product.slug} product={product} />
-          ))}
-        </div>
+          <ProductList 
+          data={sampleData} 
+          title="Product Cards" 
+          limit={4}/>
       </div>
     </>
   );
--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,5 @@
+import ProductCard from "./product-card";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const ProductList = ({
   data,
@@ -8,17 +10,15 @@ const ProductList = ({
   title?: string;
   limit?: number;
 }) => {
-  const limitedData = limit ? data.slice(0, limit) : data;
+  const limitedData = limit ? data.products.slice(0, limit) : data;
 
   return (
-    <div className="my-10">
+    <div  className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
-      {data.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {data.products.length > 0 ? (
+        <div  className="flex wrap gap-4">
           {limitedData.map((product: any) => (
-            <div key={product.name} style={{ border: "1px solid red" }}>
-              {product.slug}
-            </div>
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
