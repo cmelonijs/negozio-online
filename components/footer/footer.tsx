71c2cacc13940d12b62400759f1830bb43227729
cmelonijs <carlomeloni63@gmail.com>
2025-03-25 16:51:09 +0100

    fix: minor footer fix

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,10 +1,12 @@
+import { APP_NAME } from "@/lib/costants";
+
 const Footer = () => {
   return (
     <footer className="border-t">
       <div className="p-5 flex-center gap-2">
-        <h2>{new Date().getFullYear()}</h2>
-        <h2>Company name</h2>
-        <h2>. All right are reserved.</h2>
+        <h2>
+          {new Date().getFullYear()} {APP_NAME}. All right are reserved.
+        </h2>
       </div>
     </footer>
   );
