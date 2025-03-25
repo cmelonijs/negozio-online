db5ffdc8f92ec76473b6845260ac64a4c676786c
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-25 15:06:01 +0100

    loading created correctly

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "../globals.css";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
+import { Suspense } from "react";
+import Loading from "../loading";
 
 export const metadata: Metadata = {
   title: "Store",
@@ -16,7 +18,11 @@ export default function RootLayout({
   return (
     <div className="flex h-screen flex-col">
       <Header />
-      <main className="flex-1 wrapper">{children}</main>
+      <main className="flex-1 wrapper">
+        <Suspense fallback={<Loading />}>
+          {children}
+        </Suspense>
+      </main>
       <Footer />
     </div>
   );
