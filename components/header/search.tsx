5f4091556bbbfb58d1a3d3fe9679faf951012249
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-25 10:16:39 +0100

    feat:search-bar created

---
+++ components/header/search.tsx
@@ -0,0 +1,83 @@
+const Search = () => {
+    return (
+        <div className="hidden md:block">
+            <form action="/search" method="GET">
+                <div className="flex w-full items-center max-w-sm space-x-2">
+                    <button
+                        type="button"
+                        role="combobox"
+                        aria-expanded="false"
+                        aria-autocomplete="none"
+                        dir="ltr"
+                        data-state="closed"
+                        data-size="default"
+                        data-slot="select-trigger"
+                        aria-controls="radix-:Rcrdb"
+                        className="border-input data-[placeholder]:text-muted-foreground [&_svg:not([class*='text-'])]:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:hover:bg-input/50 flex items-center justify-between gap-2 rounded-md border px-3 py-2 text-sm whitespace-nowrap shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 data-[size=default]:h-9 data-[size=sm]:h-8 *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 bg-white dark:bg-black w-[180px]">
+                        <span
+                            data-slot="select-value"
+                            style={{ pointerEvents: 'none' }}
+                            > 
+                            camicie
+                        </span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="lucide lucide-chevron-down size-4 opacity-50"
+                            aria-hidden="true"
+                        >
+                            <path d="m6 9 6 6 6-6"></path>
+                        </svg>
+                    </button>
+                    <select aria-hidden="true" tabIndex={-1} name="category"
+                        style={{
+                            position: 'absolute',
+                            border: '0px',
+                            width: '1px',
+                            height: '1px',
+                            padding: '0px',
+                            margin: '-1px',
+                            overflow: 'hidden',
+                            clip: 'rect(0px, 0px, 0px, 0px)',
+                            whiteSpace: 'nowrap',
+                            overflowWrap: 'normal'
+                        }}
+                    >
+                        <option value="all">All</option>
+                        <option value="camicie">Camicie</option>
+                    </select>
+                    <input type="text" data-slot="input" className="border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive md:w-[100px] lg:w-[300px] bg-white dark:bg-black"
+                        placeholder="Search..."
+                        name="q"
+                    />
+                    <button data-slot="button" className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 h-9 px-4 py-2 has-[>svg]:px-3">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="lucide lucide-search size-4"
+                        >
+                            <circle cx="11" cy="11" r="8"></circle>
+                            <path d="m21 21-4.3-4.3"></path>
+                        </svg>
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Search;
