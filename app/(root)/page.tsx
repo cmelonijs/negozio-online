b32ca30d0f85c2cc8293b6411cf3743f58ceff25
cmelonijs <carlomeloni63@gmail.com>
2025-03-25 14:41:57 +0100

    feat: add sample data and showing theme inside a list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const RootPage = () => {
-  return <>Root page</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={2}
+      />
+    </>
+  );
 };
 
 export default RootPage;
