a05ff76a88231fbd3fd34b164fca287c2e6c45bb
cmelonijs <carlomeloni63@gmail.com>
2025-03-25 17:12:06 +0100

    fix: minor header fixes

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,6 +1,5 @@
 import Shopping from "@/components/header/Shopping";
 import User from "@/components/header/User";
-import MenuButton from "./menuButton";
 import LogoDiv from "./logoDiv";
 import Search2 from "./search";
 import ModeToggle from "./mode-toggle";
@@ -8,11 +7,8 @@ import MobileMenu from "./mobileMenu";
 
 const Header = () => {
   return (
-    <div className="wrapper flex items-center w-full h-[100px] px-4">
+    <div className="wrapper flex flex-row items-center justify-around w-full h-[120px] px-4">
       <div className="first-div flex items-center h-full flex-shrink-0 justify-start">
-        <div className="flex h-full items-center">
-          <MenuButton />
-        </div>
         <div className="flex h-full justify-start">
           <LogoDiv />
         </div>
@@ -22,7 +18,7 @@ const Header = () => {
           <Search2 />
         </div>
       </div>
-      <div className="third-div flex h-full items-center gap-1 justify-end">
+      <div className="third-div hidden md:flex  h-full items-center gap-1 justify-end">
         <div className="hidden md:flex items-center gap-1">
           <ModeToggle />
           <div className="cart">
@@ -32,9 +28,9 @@ const Header = () => {
             <User />
           </div>
         </div>
-        <div className="md:hidden">
-          <MobileMenu />
-        </div>
+      </div>
+      <div className="md:hidden">
+        <MobileMenu />
       </div>
     </div>
   );
