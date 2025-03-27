14ee0872ede72a62f506e37b7af8d94a80272b8a
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-27 14:05:17 +0100

    feat:productDetails with data

--- app/(root)/products/[slug]/page.tsx
+++ app/(root)/products/[slug]/page.tsx
@@ -1,7 +1,37 @@
-const ProductDetailsPage = () => {
-    return ( 
-        <h1>Esta es la pagina de detalles</h1>
-     );
+import { notFound } from "next/navigation";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function ProductPage({ params }: { params: { slug: string } }) {
+  const product = await prisma.product.findUnique({
+    where: { slug: params.slug },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex flex-col gap-8 md:gap-12 lg:gap-16 max-w-screen-xl m-auto px-4 md:px-8 lg:px-12 xl:px-16 py-8 md:py-12 lg:py-16 xl:py-20">
+      <section className="flex flex-col items-start gap-8 md:flex-row md:gap-4 xl:gap-6">
+        {/* Galería de imágenes */}
+        <div className="w-full md:w-1/2">
+        {/* Aqui va el componente de la imagen */}
+        </div>
+
+        {/* Detalles del producto */}
+        <div className="w-full md:w-1/2">
+          <h1 className="text-3xl font-bold">{product.name}</h1>
+          <p className="text-lg">{product.description}</p>
+          <p className="text-lg font-bold">Precio: ${product.price.toString()}</p>
+          <p className="text-lg">Calificación: {product.rating.toString()} / 5</p>
+          <p className="text-lg">Revisiónes: {product.numReviews}</p>
+          <p className="text-lg">Stock: {product.stock}</p>
+          <p className="text-lg">Categoría: {product.category}</p>
+          <p className="text-lg">Marca: {product.brand}</p>
+        </div>
+      </section>
+    </div>
+  );
 }
- 
-export default ProductDetailsPage;
