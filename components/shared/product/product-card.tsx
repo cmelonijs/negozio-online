499ac45880910e37292c07628983df0001c42661
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-27 11:14:49 +0100

    fix:Home page product card css

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -14,14 +14,18 @@ import {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const ProductCard = ({ product }: any) => {
   return (
-    <Card key={product.slug}>
-      <Image
-        src={product.images[0]}
-        width={250}
-        height={500}
-        className="object-cover"
-        alt={product.name} 
-      />
+    <Card 
+      key={product.slug} 
+      className="w-64 h-auto shadow-md"
+    >
+      <div className="relative w-full h-48">
+        <Image
+          src={product.images[0]}
+          layout="fill"
+          className="object-cover rounded-t-md"
+          alt={product.name}
+        />
+      </div>
       <CardHeader>
         <CardTitle>{product.name}</CardTitle>
         <CardDescription>{product.description}</CardDescription>
@@ -29,7 +33,7 @@ const ProductCard = ({ product }: any) => {
       <CardContent>
         <ProductPrice value={product.price}/>
       </CardContent>
-      <CardFooter>
+      <CardFooter className="flex justify-center gap-1">
         <Star/><Star/><Star/><Star/><Star/>
       </CardFooter>
     </Card>
--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -17,7 +17,9 @@ const ProductList = ({
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
-        <div className="flex wrap gap-4">
+        <div 
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
+        >
           {limitedData.map((product: any) => (
             <ProductCard key={product.slug} product={product} />
           ))}
