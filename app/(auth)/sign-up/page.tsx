5accaef9ea3be6c1b0711e6abe0b8dade5e01c27
Alba Castilla <castillamackin@gmail.com>
2025-03-28 11:52:21 +0100

    feat: visual part of login and signup

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -6,8 +6,16 @@ export default function SignIn() {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-sm mx-auto bg-white p-8 rounded-lg shadow-lg space-y-6">
-        <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">Sign In</h1>
+        <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">Sign up</h1>
         <form className="space-y-4">
+          <Input
+            name="name"
+            placeholder="Name"
+            type="text"
+            required
+            autoComplete="name"
+            className="w-full p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
           <Input
             name="email"
             placeholder="Email"
@@ -25,14 +33,14 @@ export default function SignIn() {
             className="w-full p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <Button className="w-full p-3 rounded-lg bg-gray-600 text-white hover:bg-amber-400 focus:outline-none">
-            Sign In
+            Sign Up
           </Button>
         </form>
 
         <div className="text-center">
           <Button asChild variant="link">
             <Link href="/sign-up" className="text-sm text-blue-500 hover:underline">
-              Don&apos;t have an account? Sign up
+              Already have an account? Sign up
             </Link>
           </Button>
         </div>
