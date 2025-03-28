6c71ebffd781d0e536585c0fa7c9f5f21b6842dd
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-28 10:49:15 +0100

    feat: login and logout methods created

--- lib/validators.ts
+++ lib/validators.ts
@@ -20,3 +20,8 @@ export const insertProductSchema = z.object({
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
 });
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
