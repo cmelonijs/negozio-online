201a3550274e1c6cb9e50b87b048e57dac521e28
Alba Castilla <castillamackin@gmail.com>
2025-03-28 12:14:46 +0100

    fix: imported wrong link

--- components/header/User.tsx
+++ components/header/User.tsx
@@ -1,15 +1,15 @@
-import { UserRound } from 'lucide-react';
-import { Link } from 'lucide-react';
+import { UserRound } from "lucide-react";
+import Link from "next/link";
 
 const UserRoundIcon = () => {
   return (
-    <div className="div-user flex items-centerinline hover:outline-stone-500 hover:bg-stone-200 gap-1 p-3 rounded-md bg-stone-100  hover:cursor-pointer hover:shadow-5 ">
-    <Link href="/sign-in">{/* It needs to check if user is already authorised still*/}
-    <UserRound color="black" size={18}/>
-    <p className='text-xs'>Sign In</p>
+    <Link href="/sign-in">
+      {/* It needs to check if user is already authorised still*/}
+      <div className="div-user flex items-centerinline hover:outline-stone-500 hover:bg-stone-200 gap-1 p-3 rounded-md bg-stone-100  hover:cursor-pointer hover:shadow-5 ">
+        <UserRound color="black" size={18} />
+        <p className="text-xs">Sign In</p>
+      </div>
     </Link>
-  </div>
-    
   );
 };
 
