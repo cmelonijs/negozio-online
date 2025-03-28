84e1883b8ca94c6a64c09a3b3703d812b20d25e5
Alba Castilla <castillamackin@gmail.com>
2025-03-28 15:30:51 +0100

    feat: backend sign up

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,35 @@ import { signInFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
+export async function signUpWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    await signIn("credentials", user);
+
+    return {
+      success: true,
+      message: "Signed up successfully!",
+    };
+  } catch (err) {
+    if (isRedirectError(err)) {
+      throw err;
+    }
+
+    return {
+      success: false,
+      message: "Invalid email or password",
+    };
+  }
+}
+
 // sign in the user with credentials
 export async function signInWithCredentials(
   prevState: unknown,
