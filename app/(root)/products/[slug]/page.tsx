e06bd77f812d746ae24b8ff906f3ec54f9823b5c
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-31 12:31:55 +0200

    Revert "Delete app/(root)/products/[slug]/page.tsx"
    This reverts commit d63028ba7bf438602cb4c83e0fac2c20db4a90e3.

---
+++ app/(root)/products/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import { notFound } from "next/navigation";
+import ProductImage from '@/components/shared/product/product.images'
+import { getProductBySlug } from "@/lib/actions/products.actions";
+
+
+export default async function ProductPage(props: { params: Promise<{ slug: string }>; }) {
+  const { slug } = await props.params;
+  const product = await getProductBySlug(slug);
+
+  if (!product) notFound();
+
+  return (
+    <div className="flex flex-col gap-8 md:gap-12 lg:gap-16 max-w-screen-xl m-auto px-4 md:px-8 lg:px-12 xl:px-16 py-8 md:py-12 lg:py-16 xl:py-20">
+      <section className="flex flex-col items-start gap-8 md:flex-row md:gap-4 xl:gap-6">
+        <div className="w-full md:w-1/2">
+          <ProductImage value={product.images} />
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <h1 className="text-3xl font-bold">{product.name}</h1>
+          <p className="text-lg">{product.description}</p>
+          <p className="text-lg font-bold">Precio: ${product.price.toString()}</p>
+          <p className="text-lg">Calificación: {product.rating.toString()} / 5</p>
+          <p className="text-lg">Revisiónes: {product.numReviews}</p>
+          <p className="text-lg">Categoría: {product.category}</p>
+          <p className="text-lg">Marca: {product.brand}</p>
+
+          <div className="mt-6 p-4 border rounded-lg">
+            <p className="text-xl font-bold mb-2">${product.price.toString()}</p>
+            <p className="text-lg mb-2">
+              Status: <span className={product.stock > 0 ? "text-green-600" : "text-red-600"}>
+                {product.stock > 0 ? "In Stock" : "Out of Stock"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
