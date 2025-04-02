795681167c05f91264e304e643bd9581420a7d91
Alba Castilla <castillamackin@gmail.com>
2025-04-02 15:06:19 +0200

    feat: loading page in root folder

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import loader from "@/assets/loader.gif";
+const LoadingPage = () => {
+    return (
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            height:"100vh",
+            width:"100vw"
+        }}>
+            <Image src={loader} height={200} width={200} alt="Loading..." />
+        </div>
+    );
+}
+
+export default LoadingPage;
