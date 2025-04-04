dc35d51cd87374440de31df7f10495a546c7a039
Alba Castilla <castillamackin@gmail.com>
2025-04-04 11:40:38 +0200

    feat: printing shipping address in order

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -2,16 +2,20 @@ import CheckoutSteps from "@/components/shared/checkout-steps";
 import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@/auth";
 import OrderPaymentMethodPage from "./orderPaymentMethod";
+import { ShippingAddress } from "@/types";
+import OrderShippingAdressPage from "./orderShippingAddress";
 
 const PaymentMethodPage = async () => {
   const session = await auth();
   const userId = session?.user?.id;
   let userPaymentMethod = null;
+  let shippingAddress: ShippingAddress | null = null;
 
   if (userId) {
     try {
       const user = await getUserById(userId);
       userPaymentMethod = user.paymentMethod || null;
+      shippingAddress = user?.address as ShippingAddress || null;
     } catch (error) {
       console.error("Error fetching user:", error);
     }
@@ -20,6 +24,7 @@ const PaymentMethodPage = async () => {
     <>
       <CheckoutSteps current={3} />
       <OrderPaymentMethodPage paymentMethod={userPaymentMethod} />;
+      <OrderShippingAdressPage initialShippingForm={shippingAddress || undefined} />
     </>
   );
 };
