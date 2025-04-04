20e980aa167d561e3f63ec920097bad917de8301
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-04-04 11:12:26 +0200

    feat: paymentMethod rendered in place-order

---
+++ app/(root)/place-order/orderPaymentMethod.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+interface PaymentMethodFormProps {
+    paymentMethod: string | null;
+}
+
+export default function OrderPaymentMethodPage({ paymentMethod }: PaymentMethodFormProps) {
+    return (
+        <div className="max-w-md mx-auto space-y-4 p-6 bg-white shadow-lg rounded-lg">
+            <h2 className="text-xl font-semibold text-gray-800">Payment Method</h2>
+            
+            {paymentMethod ? (
+                <div className="mt-4">
+                    <p className="text-gray-700">
+                        <span className="font-medium">Selected payment method:</span> {paymentMethod}
+                    </p>
+                </div>
+            ) : (
+                <p className="text-amber-600">No payment method selected yet.</p>
+            )}
+        </div>
+    );
+}
--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -1,14 +1,25 @@
 import CheckoutSteps from "@/components/shared/checkout-steps";
 import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@/auth";
+import OrderPaymentMethodPage from "./orderPaymentMethod";
 
 const PaymentMethodPage = async () => {
   const session = await auth();
   const userId = session?.user?.id;
+  let userPaymentMethod = null;
+
+  if (userId) {
+    try {
+      const user = await getUserById(userId);
+      userPaymentMethod = user.paymentMethod || null;
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  }
   return (
     <>
       <CheckoutSteps current={3} />
-      
+      <OrderPaymentMethodPage paymentMethod={userPaymentMethod} />;
     </>
   );
 };
