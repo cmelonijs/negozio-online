a56e69f3105094ef81381c1af2835026cd79f2e3
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-04-04 11:23:23 +0200

    fix: link to payment-method page

--- app/(root)/place-order/orderPaymentMethod.tsx
+++ app/(root)/place-order/orderPaymentMethod.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+
 interface PaymentMethodFormProps {
     paymentMethod: string | null;
 }
@@ -8,7 +11,7 @@ export default function OrderPaymentMethodPage({ paymentMethod }: PaymentMethodF
     return (
         <div className="max-w-md mx-auto space-y-4 p-6 bg-white shadow-lg rounded-lg">
             <h2 className="text-xl font-semibold text-gray-800">Payment Method</h2>
-            
+
             {paymentMethod ? (
                 <div className="mt-4">
                     <p className="text-gray-700">
@@ -18,6 +21,12 @@ export default function OrderPaymentMethodPage({ paymentMethod }: PaymentMethodF
             ) : (
                 <p className="text-amber-600">No payment method selected yet.</p>
             )}
+            <Button
+                onClick={() => redirect("/payment-method")}
+                variant="outline"
+            >
+                Edit
+            </Button>
         </div>
     );
 }
