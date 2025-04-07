6faccb5692bb0beba6bd102674604d2fa68f9968
Alba Castilla <castillamackin@gmail.com>
2025-04-07 15:32:19 +0200

    fix: loader centered in user page

--- app/user/loading.tsx
+++ app/user/loading.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import loader from "@/assets/loader.gif";
+
 const LoadingPage = () => {
-    return (
-        <div style={{
-            display:"flex",
-            justifyContent:"center",
-            alignItems:"center",
-            height:"100vh",
-            width:"100vw"
-        }}>
-            <Image src={loader} height={200} width={200} alt="Loading..." />
-        </div>
-    );
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", 
+        width: "100vw",
+        position: "fixed", 
+        top: 0,
+        left: 0,
+        backgroundColor: "white",
+        zIndex: 9999, 
+      }}
+    >
+      <Image src={loader} height={200} width={200} alt="Loading..." />
+    </div>
+  );
+};
 
 export default LoadingPage;
