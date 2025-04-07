f038d96cc8684825fd8083fc9bd8f01c618beddf
Alba Castilla <castillamackin@gmail.com>
2025-04-07 15:24:05 +0200

    fix: eliminated comments i had written for myseld

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -2,14 +2,14 @@ import { getMyOrders } from "@/lib/actions/order.actions";
 import MyOrdersTable from "./orders-table";
 
 const OrdersPage = async () => {
-  // Llamamos a la función para obtener los pedidos del usuario
+  
   const { data: rawOrders, totalPages } = await getMyOrders({ limit: 4, page: 1 });
 
-  // Convertimos los datos para que sean compatibles con el componente cliente
+ 
   const orders = rawOrders.map(order => ({
     id: order.id,
-    createdAt: order.createdAt.toISOString(), // Convertimos la fecha a string
-    totalPrice: Number(order.totalPrice), // Convertimos Decimal a número
+    createdAt: order.createdAt.toISOString(), 
+    totalPrice: Number(order.totalPrice), 
     isPaid: order.isPaid,
     isDelivered: order.isDelivered,
   }));
