e6a792c35be039afc5e4aad985e2a7f925504016
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-04-07 12:28:57 +0200

    feat: base layout structure created

---
+++ app/user/orders/page.tsx
@@ -0,0 +1,7 @@
+const OrdersPage = () => {
+    return ( 
+        <h1>This is the order page</h1>
+     );
+}
+ 
+export default OrdersPage;
---
+++ app/user/page.tsx
@@ -0,0 +1,10 @@
+const HomePage = async () => {
+
+  return (
+    <div>
+        <h1>Pagina de user</h1>
+    </div>
+  );
+};
+
+export default HomePage;
