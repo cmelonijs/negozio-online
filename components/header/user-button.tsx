d334df7a6c1912950f1ad935761e8bb922e4276c
Alba Castilla <castillamackin@gmail.com>
2025-04-15 14:15:25 +0200

    feat: Admin dropdown user button checks role of user authenticated and only appears if the user is an admin

--- components/header/user-button.tsx
+++ components/header/user-button.tsx
@@ -10,6 +10,7 @@ import {
 import { signOutUser } from "@/lib/actions/user.actions";
 import { UserIcon } from "lucide-react";
 import Link from "next/link";
+import { isAdminUser } from "@/lib/actions/user.actions";
 
 const UserButton = async () => {
   const session = await auth();
@@ -25,6 +26,7 @@ const UserButton = async () => {
   }
 
   const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? "U";
+  const isAdmin = await isAdminUser();
 
   return (
     <div className="flex gap-2 items-center">
@@ -60,14 +62,16 @@ const UserButton = async () => {
             </Link>
           </DropdownMenuItem>
 
-          <DropdownMenuItem asChild className="border border-gray-300 rounded-sm pt-7 pb-7 pl-6 mb-1">
-            <Link
-              href="/admin/overview"
-              className="w-full py-4 px-2 h-4 justify-start"
-            >
-              Admin
-            </Link>
-          </DropdownMenuItem>
+          {isAdmin && (
+            <DropdownMenuItem asChild className="border border-gray-300 rounded-sm pt-7 pb-7 pl-6 mb-1">
+              <Link
+                href="/admin/overview"
+                className="w-full py-4 px-2 h-4 justify-start"
+              >
+                Admin
+              </Link>
+            </DropdownMenuItem>
+          )}
 
           <DropdownMenuItem asChild className="border border-gray-300 rounded-sm p-3 mb-1">
             <form action={signOutUser} className="w-full">
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -194,3 +194,19 @@ export async function updateProfile(user: { name: string; email: string }) {
     };
   }
 }
+
+export async function isAdminUser(): Promise<boolean> {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    return false;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { id: session.user.id },
+    select: { role: true },
+  });
+
+  return user?.role === "admin";
+}
+
