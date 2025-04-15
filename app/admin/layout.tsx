503e75745244b46f51d5693d52eeb20c37b0645d
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-04-15 10:11:46 +0200

    feat:admin layout and header created

---
+++ app/admin/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import UserHeader from "./header";
+
+export const metadata: Metadata = {
+    title: "Admin area",
+    description: "E-commerce",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="flex h-screen flex-col">
+            <UserHeader />
+            <main className="flex-1 wrapper">{children}</main>
+        </div>
+    );
+}
---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,7 @@
+const OrdersPage = () => {
+    return ( 
+        <h1>This is the orders page</h1>
+     );
+}
+ 
+export default OrdersPage;
