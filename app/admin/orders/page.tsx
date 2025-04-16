60e522d5ada68c254592c0913cb66381251cf9b2
Pamela-git <pamela.ladu@gmail.com>
2025-04-16 12:06:04 +0200

    added order type

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,13 +1,50 @@
 import {getAllOrders} from "@/lib/actions/order.actions";
 import {Fragment} from "react";
 import OrdersTable from "./order-table";
+import {Order} from "@/types";
 
 const OrdersPage = async ({ searchParams }: { searchParams: Promise<Record<string, string>> }) => {
     const resolvedSearchParams = await searchParams;
     const currentPage = Number(resolvedSearchParams.page) || 1;
 
 
-    const { data: orders, totalPages } = await getAllOrders({ limit: 10, page: currentPage, query: 'all' });
+    const { data: ordersData, totalPages } = await getAllOrders({ limit: 10, page: currentPage, query: 'all' });
+
+    const orders: Order[] = ordersData.map((order) => {
+        const parsedAddress = typeof order.shippingAddress === 'string'
+            ? JSON.parse(order.shippingAddress)
+            : order.shippingAddress;
+
+        return {
+            id: order.id,
+            createdAt: new Date(order.createdAt),
+            shippingAddress: {
+                fullName: parsedAddress?.fullName ?? "",
+                streetAddress: parsedAddress?.streetAddress ?? "",
+                city: parsedAddress?.city ?? "",
+                postalCode: parsedAddress?.postalCode ?? "",
+                Country: parsedAddress?.Country ?? "",
+                lat: parsedAddress?.lat,
+                lng: parsedAddress?.lng,
+            },
+            paymentMethod: order.paymentMethod,
+            PaymentResult: order.PaymentResult,
+            itemsPrice: order.itemsPrice.toString(),
+            totalPrice: order.totalPrice.toString(),
+            shippingPrice: order.shippingPrice.toString(),
+            taxPrice: order.taxPrice.toString(),
+            isPaid: order.isPaid,
+            paidAt: order.paidAt ? new Date(order.paidAt) : null,
+            isDelivered: order.isDelivered,
+            deliveredAt: order.deliveredAt ? new Date(order.deliveredAt) : null,
+            orderItems: [],
+            user: {
+                name: order.user.name ? order.user.name : '',
+                email: "",
+            },
+            userId: order.userId,
+        };
+    });
 
     return (
         <Fragment>
