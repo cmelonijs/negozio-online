af76b72117634c987cc55b1c349531d9affb05c0
Alba Castilla <castillamackin@gmail.com>
2025-04-17 16:00:20 +0200

    feat: restructuration of page to look more like the og

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,7 +1,7 @@
 import { countAllProducts } from "@/lib/actions/products.actions";
 import { countUsersWithRole } from "@/lib/actions/user.actions";
 import { countAllOrders } from "@/lib/actions/order.actions";
-import { getRecentSales } from "@/lib/actions/order.actions"; 
+import { getRecentSales } from "@/lib/actions/order.actions";
 import { getTotalRevenue } from "@/lib/actions/order.actions";
 import ProductBox from "./products-box";
 import CustomersBox from "./customers-box";
@@ -11,39 +11,45 @@ import RevenueBox from "./revenue-box";
 
 type Sale = {
   name: string;
-  date: string; 
-  totalPrice: number;  
+  date: string;
+  totalPrice: number;
 };
 
 const OverviewsPage = async () => {
-    const { total: totalProducts } = await countAllProducts();
-    const { total: totalUsers } = await countUsersWithRole("user");
-    const { totalOrders } = await countAllOrders();
-    const salesResponse = await getRecentSales();
-    const { totalRevenue } = await getTotalRevenue(); // <-- Aquí
-  
-    let formattedSales: Sale[] = [];
-  
-    if (Array.isArray(salesResponse)) {
-      formattedSales = salesResponse.map((sale) => ({
-        name: sale.name,
-        date: sale.date.toISOString(),
-        totalPrice: parseFloat(sale.totalPrice.toString()),
-      }));
-    }
-  
-    return (
-      <>
-        <h1 className="text-2xl font-semibold mb-4">This is the overview page</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-7">
-          <ProductBox total={typeof totalProducts === "number" ? totalProducts : null} />
-          <CustomersBox total={typeof totalUsers === "number" ? totalUsers : null} />
-          <SalesBox totalOrders={typeof totalOrders === "number" ? totalOrders : null} />
-          <RevenueBox totalRevenue={totalRevenue ?? 0} />
-        </div>
-        <RecentSales sales={formattedSales} />
-      </>
-    );
-  };
-  
-  export default OverviewsPage;
+  const { total: totalProducts } = await countAllProducts();
+  const { total: totalUsers } = await countUsersWithRole("user");
+  const { totalOrders } = await countAllOrders();
+  const salesResponse = await getRecentSales();
+  const { totalRevenue } = await getTotalRevenue();
+
+  let formattedSales: Sale[] = [];
+
+  if (Array.isArray(salesResponse)) {
+    formattedSales = salesResponse.map((sale) => ({
+      name: sale.name,
+      date: sale.date.toISOString(),
+      totalPrice: parseFloat(sale.totalPrice.toString()),
+    }));
+  }
+
+  return (
+    <>
+      <h1 className="text-2xl font-semibold mb-4">This is the overview page</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-7">
+        <RevenueBox totalRevenue={totalRevenue ?? 0} />
+        <SalesBox
+          totalOrders={typeof totalOrders === "number" ? totalOrders : null}
+        />
+        <CustomersBox
+          total={typeof totalUsers === "number" ? totalUsers : null}
+        />
+        <ProductBox
+          total={typeof totalProducts === "number" ? totalProducts : null}
+        />
+      </div>
+      <RecentSales sales={formattedSales} />
+    </>
+  );
+};
+
+export default OverviewsPage;
--- app/admin/overview/revenue-box.tsx
+++ app/admin/overview/revenue-box.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
-import { DollarSign } from "lucide-react";
+import { BadgeDollarSign } from 'lucide-react';
 
 type Props = {
   totalRevenue: number;
@@ -12,7 +12,7 @@ const RevenueBox = ({ totalRevenue }: Props) => {
     <Card className="p-4 flex flex-col justify-between h-36 rounded-lg border transition-all duration-300 hover:border-gray-500 dark:hover:border-white">
       <div className="flex items-start justify-between">
         <h2 className="text-lg font-semibold ml-1">Revenue</h2>
-        <DollarSign className="w-7 h-7 stroke-[2.5] text-black dark:text-white mr-1" />
+        <BadgeDollarSign className="w-7 h-7 stroke-[2.5] text-black dark:text-white mr-1" />
       </div>
       <p className="text-4xl font-bold text-center mt-2 mb-4">
         {totalRevenue.toFixed(2)}€
