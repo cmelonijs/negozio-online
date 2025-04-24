44c992020f9de17667900a7c6dc9485fa1437a27
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-04-24 17:16:44 +0200

    feat:role checked in admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,17 +2,31 @@ import type { Metadata } from "next";
 import "../globals.css";
 import UserHeader from "./header";
 import { DynamicBreadcrumbs } from "@/components/shared/breadcrumb";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import { getUserRole } from "@/lib/actions/user.actions";
 
 export const metadata: Metadata = {
     title: "Admin area",
     description: "E-commerce",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+  if (!session?.user || !userId) {
+    redirect('/sign-in');
+  }
+  
+  const role = await getUserRole(userId);
+  
+  if (role !== 'admin') {
+    redirect('/unauthorized');
+  }
     return (
         <div className="flex h-screen flex-col">
             <UserHeader />
