186b94f4e59bef8aaabbd4cf10f5214b27269b5f
Alba Castilla <castillamackin@gmail.com>
2025-05-07 11:27:10 +0200

    feat:graph presentarion to look more like model

--- app/admin/overview/graph.tsx
+++ app/admin/overview/graph.tsx
@@ -1,21 +1,18 @@
-"use client"
-import { TrendingUp } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, XAxis, Tooltip } from "recharts";
+"use client";
+
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis} from "recharts";
 
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  ChartContainer,
-} from "@/components/ui/chart";
+import { ChartContainer } from "@/components/ui/chart";
 
 type GraphProps = {
-  data: { month: string; total: number }[]; // The data prop that accepts an array of month/total
+  data: { month: string; total: number }[]; 
 };
 
 export function Graph({ data }: GraphProps) {
@@ -26,7 +23,9 @@ export function Graph({ data }: GraphProps) {
         <CardDescription>Sales in the last couple of months</CardDescription>
       </CardHeader>
       <CardContent>
-        <ChartContainer config={{ type: { label: "Bar Chart", color: "blue" }, options: {} }}>
+        <ChartContainer
+          config={{ type: { label: "Bar Chart" }, options: {} }}
+        >
           <BarChart data={data}>
             <CartesianGrid vertical={false} />
             <XAxis
@@ -34,21 +33,19 @@ export function Graph({ data }: GraphProps) {
               tickLine={false}
               tickMargin={10}
               axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)} // Display the first 3 letters of the month
+              tickFormatter={(value) => value}
             />
-            <Tooltip />
+            <YAxis
+              tickLine={false}
+              axisLine={false}
+              tickMargin={10}
+              tickFormatter={(value) => `$${value}`} 
+            />
+
             <Bar dataKey="total" fill="var(--color-desktop)" radius={8} />
           </BarChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total sales for the last few months
-        </div>
-      </CardFooter>
     </Card>
   );
 }
